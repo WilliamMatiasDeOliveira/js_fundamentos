1e40f07c8298628955e99f3b09778ec448ec8eae
WilliamMatiasDeOLiveira <williammatiasjau@hotmail.com>
2025-08-08 21:31:09 -0300

    switch

---
+++ switch.js
@@ -0,0 +1,15 @@
+let opcao = 2
+
+switch (opcao){
+    case 1:
+        console.log("opção 1")
+        break
+    case 2:
+        console.log("opção 2")
+        break
+    case 3:
+        console.log('opção 3')
+        break
+    default:
+        console.log('opcao invalida')
+}
