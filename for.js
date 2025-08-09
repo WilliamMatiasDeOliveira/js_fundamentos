1f37f596f78ef97aa527cbc98c4acfc81a757932
WilliamMatiasDeOLiveira <williammatiasjau@hotmail.com>
2025-08-09 07:08:41 -0300

    for

---
+++ for.js
@@ -0,0 +1,9 @@
+
+
+for (let i = 1; i <= 10; i++) {
+    console.log(`TABUADA DO ${i}`)
+    for (let j = 1;j <= 10; j++){
+        let res = i * j
+        console.log(`${i} x ${j} = ${res}`)
+    }
+}
