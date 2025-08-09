7b93c09c6ca93c21125e6450a72ceb1cfafb3f92
WilliamMatiasDeOLiveira <williammatiasjau@hotmail.com>
2025-08-08 21:25:09 -0300

    if_else_if_else

---
+++ if_else_if_else.js
@@ -0,0 +1,9 @@
+let valor = 15
+
+if(valor < 15){
+    console.log('o valor é menor que 15')
+}else if(valor > 15){
+    console.log('seu valor é maior que 15')
+}else{
+    console.log("o valor é igual a 15")
+}
