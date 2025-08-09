9a21324a20805b55dec8f2a7b480edca2342c913
WilliamMatiasDeOLiveira <williammatiasjau@hotmail.com>
2025-08-09 07:55:09 -0300

    while

---
+++ while.js
@@ -0,0 +1,29 @@
+
+// let numero = 0
+
+// while(numero <= 10){
+//     console.log(numero)
+//     numero++
+// }
+
+// let numero = 0
+
+// while (numero <= 10) {
+//     numero++
+//     if (numero == 5) {
+//         continue
+//     }
+//     console.log(numero)
+// }
+
+let numero = 0
+
+while(numero <= 10){
+    numero++
+    if(numero == 5){
+        break
+    }
+
+    console.log(numero)
+}
+
