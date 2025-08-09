73c2208b2940fde0a1529587b24684e76180e850
WilliamMatiasDeOLiveira <williammatiasjau@hotmail.com>
2025-08-09 09:09:16 -0300

    objeto construtor

--- objetos/objeto_construtor.js
+++ objetos/objeto_construtor.js
@@ -3,6 +3,9 @@ function Pessoa(nome, sobrenome, idade){
     this.nome = nome
     this.sobrenome = sobrenome
     this.idade = idade
+    this.nome_completo = function(){
+        return `${this.nome} ${this.sobrenome}`
+    }
 }
 
 pessoa = new Pessoa("william", "matias", 42)
@@ -10,3 +13,4 @@ pessoa = new Pessoa("william", "matias", 42)
 console.log(pessoa.nome)
 console.log(pessoa.sobrenome)
 console.log(pessoa.idade)
+console.log(pessoa.nome_completo())
