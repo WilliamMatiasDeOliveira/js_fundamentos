8ff18542ee3ed4b9e3fa7a89257402be2c5f5194
WilliamMatiasDeOLiveira <williammatiasjau@hotmail.com>
2025-08-09 07:37:19 -0300

    for of

---
+++ for_of.js
@@ -0,0 +1,19 @@
+
+const numeros = [10, 20, 30];
+
+const person = [
+    {fname:"John", lname:"Doe", age:25},
+    {fname:"maria", lname:"silva", age:23}
+]
+
+// for(x of numeros){
+//     console.log(x)
+//     console.log(numeros[x])
+// }
+
+for(x of person){
+    console.log(x)
+    console.log(x.fname)
+}
+
+
