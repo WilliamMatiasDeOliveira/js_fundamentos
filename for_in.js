85f1d3d2170005cf7bcc892454d69a73ba469f23
WilliamMatiasDeOLiveira <williammatiasjau@hotmail.com>
2025-08-09 07:34:51 -0300

    for in

---
+++ for_in.js
@@ -0,0 +1,18 @@
+
+// const person = [
+//     {fname:"John", lname:"Doe", age:25},
+//     {fname:"maria", lname:"silva", age:23}
+
+// ]
+
+// for(p in person){
+//     console.log(person[1].fname)
+// }
+
+////////////////////////////////////////////////////////////////
+
+// const numeros = [10, 20, 30];
+
+// for(i in numeros){
+//     console.log(`indice ${i} valor ${numeros[i]}`)
+// }
